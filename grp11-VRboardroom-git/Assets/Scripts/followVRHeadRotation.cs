98eb63f0fe98ce836ec3d009a6871d85cb5654dc
knap301 <38322431+knap301@users.noreply.github.com>
2018-05-14 19:08:25 +1000

    add initial project

---
+++ grp11-VRboardroom-git/Assets/Scripts/followVRHeadRotation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followVRHeadRotation : MonoBehaviour {
+
+	public Transform vrPlayerObject;
+	public float cameraOffsetXZ;
+	public float cameraOffsetY;
+	public float bodyTurnAngle = 20f;
+
+	private Animator myAnim;
+	private Vector3 vrRot, myRot;
+	private Transform vrCamera;
+	private float x, z;
+
+
+	// Use this for initialization
+	void Start () {
+		myAnim = GetComponent<Animator> ();
+		vrCamera = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vrRot = vrCamera.rotation.eulerAngles;
+		myRot = transform.rotation.eulerAngles;
+
+		if (Mathf.DeltaAngle(vrRot.y, myRot.y) > bodyTurnAngle) {
+			if (!gameObject.GetComponent<iTween> ()) {
+				//Debug.Log("Turn LEFT");
+				//myAnim.SetTrigger ("TurnLeft");
+				iTween.RotateTo (gameObject, new Vector3 (myRot.x, vrRot.y, myRot.z), 0.75f);
+			}
+		} else if (Mathf.DeltaAngle(vrRot.y, myRot.y) < -bodyTurnAngle) {
+			if (!gameObject.GetComponent<iTween> ()) {
+				//Debug.Log("Turn RIGHT");
+				//myAnim.SetTrigger ("TurnRight");
+				iTween.RotateTo (gameObject, new Vector3 (myRot.x, vrRot.y, myRot.z), 0.75f);
+			}
+		}
+	}
+
+	void LateUpdate() {
+		x = cameraOffsetXZ * Mathf.Sin (vrCamera.rotation.eulerAngles.y * Mathf.Deg2Rad);
+		z = cameraOffsetXZ * Mathf.Cos (vrCamera.rotation.eulerAngles.y * Mathf.Deg2Rad);
+		vrPlayerObject.position = transform.position + new Vector3 (x, cameraOffsetY, z);
+	}
+
+}
---
+++ grp11-VRboardroom-git/Assets/Scripts/sitOn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sitOn : MonoBehaviour {
+
+	Animator myAnim;
+	public GameObject player;
+	//public Transform seatPos;
+
+	// Use this for initialization
+	void Start () {
+		myAnim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		/*
+		if (Vector3.Distance(myPlayer.transform.position, mySeat.transform.position) < 0.5) {
+
+			myAnim.SetTrigger ("Sit");
+
+			myPlayer.transform.rotation = mySeat.transform.rotation;
+			myAnim.SetBool ("isSitting", true);
+		} else myAnim.SetBool("isSitting", false);
+		*/
+
+
+
+		if (Vector3.Distance(player.transform.position, this.transform.position) < 2) {
+
+			myAnim.SetTrigger ("isSitting");
+
+			player.transform.rotation = this.transform.rotation;
+		}
+	}
+
+}
+/*
+	public void sit() {
+		if (Input.GetMouseButtonDown (0)) {
+			player.position = seatPos.position;
+			player.rotation = seatPos.rotation;
+
+			myAnim.SetTrigger ("Sit");
+		}
+	}
+*/
+
